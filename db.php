d2749a29b566943fcdd5bb081ed7fb247bbedf21
Gerardo Lara <58283695+gerardolarx@users.noreply.github.com>
2021-08-05 01:46:32 -0500

    Update db.php

--- db.php
+++ db.php
@@ -5,7 +5,7 @@ session_start();
 $conn = mysqli_connect(
     'localhost', //donde esta el servidor de la base de datos
     'root', //usuario
-    '1234', //contraseña 
+    '', //contraseña 
     'crud' //base de datos
 );
 
